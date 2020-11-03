b17f17770f63d1fc661bd2360d8b0292de76377a
Mohamed Bashir <bashimoha3@gmail.com>
2020-11-03 14:57:49 -0600

    Category Class #4

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -1,1 +1,29 @@
+import 'package:wtm/models/event.dart';
+import 'dart:convert';
 
+List<Category> categoryFromJson(String str) =>
+    List<Category>.from(json.decode(str).map((x) => Category.fromJson(x)));
+
+class Category {
+  Category({
+    this.name,
+    this.id,
+    this.events,
+  });
+
+  String name;
+  int id;
+  List<Event> events;
+
+  factory Category.fromJson(Map<String, dynamic> json) => Category(
+        name: json["name"],
+        id: json["id"],
+        events: List<Event>.from(json["events"].map((x) => Event.fromJson(x))),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "name": name,
+        "id": id,
+        "events": List<dynamic>.from(events.map((x) => x.toJson())),
+      };
+}
